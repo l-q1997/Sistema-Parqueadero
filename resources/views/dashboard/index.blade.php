8c3034af3898b63ff413abb98e88d5df9c185669
linnerquintero <inglinnerquintero@gmail.com>
2024-10-09 12:09:53 -0500

    imagen nueva en la pagina

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -118,7 +118,7 @@
                                 </div>
                             </div>
                         </div>
-               
+
                         <div class="card-body">
                             <h6 class="mb-0 "> Daily Sales </h6>
                             <p class="text-sm "> (<span class="font-weight-bolder">+15%</span>) increase in today
@@ -131,14 +131,17 @@
                         </div>
                     </div>
                 </div>
+
                 <div class="col-lg-4 mt-4 mb-3">
                     <div class="card z-index-2 ">
                         <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
-                            <div class="bg-gradient-dark shadow-dark border-radius-lg py-3 pe-1">
-                                <div class="chart">
-                                    <canvas id="chart-line-tasks" class="chart-canvas" height="170"></canvas>
-                                </div>
-                            </div>
+                            <div 
+    class="shadow-dark border-radius-lg py-3 pe-1" 
+    style="background-image: url('{{ asset('assets') }}/img/son.jpg'); background-size: cover; background-position: center;">
+    <div class="chart">
+        <canvas id="chart-line-tasks" class="chart-canvas" height="170"></canvas>
+    </div>
+</div>
                         </div>
                         <div class="card-body">
                             <h6 class="mb-0 ">Completed Tasks</h6>
